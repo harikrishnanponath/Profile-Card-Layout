d8aced879e33f87bf1deaa14f1d27eaadafc5b79
Harikrishnan <115897436+harikrishnanponath@users.noreply.github.com>
2025-01-27 16:57:33 -0500

    Added list of users and designed the User Detail Screen

--- app/src/main/java/com/harikrish/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/harikrish/profilecardlayout/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.harikrish.profilecardlayout
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,99 +14,185 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.codingtroops.profilecardlayout.UserProfile
+import com.codingtroops.profilecardlayout.userProfileList
 import com.harikrish.profilecardlayout.ui.theme.ProfileCardLayoutTheme
+import com.harikrish.profilecardlayout.ui.theme.lightGreen200
+import com.harikrish.profilecardlayout.ui.theme.lightGreen800
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ProfileCardLayoutTheme{
-                MainScreen()
+            ProfileCardLayoutTheme {
+                UserListScreen(userProfileList)
             }
 
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color.LightGray
-    ) {
-        ProfileCard()
+fun UserListScreen(userProfiles: List<UserProfile>) {
+    Scaffold(topBar = { AppBar() }) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            color = Color.LightGray
+        ) {
+            LazyColumn {
+                items(userProfiles) { userProfile ->
+                    ProfileCard(userProfile = userProfile)
+                }
+
+            }
+        }
+    }
+
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun UserDetailScreen(userProfile: UserProfile) {
+    Scaffold(topBar = { AppBar() }) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            color = Color.LightGray
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                ProfilePicture(userProfile.pictureUrl, userProfile.status, 240.dp)
+                ProfileContent(userProfile.name, userProfile.status, Alignment.CenterHorizontally)
+            }
+        }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileCard() {
+fun AppBar() {
+    TopAppBar(
+        navigationIcon = {
+            Icon(
+                Icons.Filled.Send,
+                contentDescription = "Home",
+                Modifier.padding(horizontal = 12.dp),
+                tint = Color.White
+            )
+        },
+        title = {
+            Text(
+                text = "Messaging App",
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.White
+            )
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = lightGreen800
+        )
+
+    )
+}
+
+@Composable
+fun ProfileCard(userProfile: UserProfile) {
     Card(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
             .wrapContentHeight(align = Alignment.Top),
-        elevation = CardDefaults.cardElevation(8.dp)
+        elevation = CardDefaults.cardElevation(8.dp),
+        colors = CardDefaults.cardColors(Color.White),
+        shape = CutCornerShape(topEnd = 24.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            ProfilePicture()
-            ProfileContent()
+            ProfilePicture(userProfile.pictureUrl, userProfile.status, 72.dp)
+            ProfileContent(userProfile.name, userProfile.status, Alignment.Start)
         }
     }
 }
 
 @Composable
-fun ProfilePicture() {
+fun ProfilePicture(pictureUrl: String, online: Boolean, imageSize: Dp) {
 
     Card(
         shape = CircleShape,
-        border = BorderStroke(width = 2.dp, color = Color.Green),
+        border = BorderStroke(
+            width = 2.dp,
+            color = if (online) lightGreen200 else Color.Red
+        ),
         modifier = Modifier.padding(16.dp),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Image(
-            painter = painterResource(R.drawable.hari),
-            contentDescription = "picture of Hari",
-            modifier = Modifier.size(75.dp),
-            contentScale = ContentScale.Crop
+        AsyncImage(
+            model = pictureUrl,
+            contentDescription = "Profile Picture",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(imageSize)
         )
     }
 
 }
 
 @Composable
-fun ProfileContent() {
-    Column(modifier = Modifier.padding(8.dp)) {
+fun ProfileContent(name: String, online: Boolean, alignment: Alignment.Horizontal) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp),
+        horizontalAlignment = alignment
+    ) {
         Text(
-            text = "Harikrishnan Ponath",
-            style = MaterialTheme.typography.titleLarge
+            text = name,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = if (online) Modifier.alpha(1f)
+            else Modifier.alpha(0.5f)
         )
 
         Text(
-            text = "Active now",
+            text = if (online) "Active now" else "Offline",
             style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.alpha(0.5f)
         )
@@ -121,6 +207,14 @@ fun ProfileContent() {
 @Composable
 fun GreetingPreview() {
     ProfileCardLayoutTheme {
-        MainScreen()
+        UserListScreen(userProfileList)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserDetailPreview() {
+    ProfileCardLayoutTheme {
+        UserDetailScreen(userProfile = userProfileList[0])
     }
 }
--- app/src/main/java/com/harikrish/profilecardlayout/ui/theme/Color.kt
+++ app/src/main/java/com/harikrish/profilecardlayout/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.harikrish.profilecardlayout.ui.theme
 
+import android.provider.CalendarContract.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val lightGreen200 = Color(0xFF66BB6A)
+val lightGreen800 = Color(0xFF2E7D32)
+
+val Colors.lightGreen: Color
+    @Composable
+    get() = lightGreen200
+
+val Colors.darkGreen: Color
+    @Composable
+    get() = lightGreen800
